b77d84720784484f1dd42d64ed6f5b2dafcd620e
anishjoshi1999 <anishjoshi1999@gmail.com>
2023-08-23 17:11:39 +0545

    updated

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-const express = require("express");
-const app = express();
 const axios = require("axios");
-const port = 3000;
+
 const corn = require("node-cron");
 
-// Define a route
-app.get("/", (req, res) => {
-  res.send("Hello, Express!");
-});
 const hitCountRoute = async () => {
   try {
     let response = await axios.get(
@@ -18,11 +12,7 @@ const hitCountRoute = async () => {
     console.error("Error hitting count route:", error.message);
   }
 };
-// Start the server
-app.listen(port, () => {
-  //setInterval(hitCountRoute, 3000);
-  console.log(`Server is running on port ${port}`);
-});
+
 let times = 0;
 var task = corn.schedule("*/5 * * * * *", async () => {
   await hitCountRoute();
